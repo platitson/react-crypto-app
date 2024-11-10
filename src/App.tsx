1f3485006db6dddf405df6555575e835178e939d
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2024-11-10 17:41:14 +0100

    init ant design + add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Header, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: "calc(100vh - 64px)",
+  color: "#fff",
+  backgroundColor: "#001529",
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#1677ff",
+};
+
 function App() {
-  return <>Hello Crypto</>;
+  return (
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider width="25%" style={siderStyle}>
+          Sider
+        </Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default App;
