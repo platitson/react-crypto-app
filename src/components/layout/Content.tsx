12e6766d5d88d063e884d07645873e680560fd57
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2024-11-24 13:15:01 +0100

    Add wallet price to content

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -1,15 +1,34 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../context/cryptoContext";
 
 const AntdContent = Layout.Content;
 
 const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-    minHeight: 'calc(100vh - 64px)',
-    color: '#fff',
-    backgroundColor: '#001529',
-    padding: '1rem'
-  };
+  textAlign: "center",
+  minHeight: "calc(100vh - 64px)",
+  color: "#fff",
+  backgroundColor: "#001529",
+  padding: "1rem",
+};
 
-  export function Content() {
-    return (<AntdContent style={contentStyle}>Content</AntdContent>);
+export function Content() {
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap: any = crypto.reduce((acc, coin) => {
+    (acc as any)[coin.id] = coin.price;
+    return acc;
+  }, {});
+
+  const walletPrice = assets
+    .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+    .reduce((acc, v) => (acc += v), 0)
+    .toFixed(2);
+
+  return (
+    <AntdContent style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: "left", color: "#FFF" }}>
+        Portfolio: {walletPrice}$
+      </Typography.Title>
+    </AntdContent>
+  );
 }
